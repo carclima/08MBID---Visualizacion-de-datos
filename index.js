20813784f2c9e9696320f7ad959a95405bd36218
carclima <carlescliment@tesigandia.com>
2022-05-02 00:06:59 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Importamos w3.css
 import './w3.css';
+
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName('city');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  document.getElementById(cityName).style.display = 'block';
+}
